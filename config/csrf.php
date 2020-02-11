6c3d0a585516896ef628ac5e4487e23e69b83b67
Cuveen <60683289+cuveen@users.noreply.github.com>
2020-02-12 00:54:33 +0700

    Update csrf.php

--- config/csrf.php
+++ config/csrf.php
@@ -9,5 +9,16 @@ return [
     | Class example: Test@main
     | @var array
     */
-    'except' => []
+    'except' => [],
+    /*
+    |--------------------------------------------------------------------------
+    | CSRF Token Lifetime
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the number of minutes that you wish the session
+    | to be allowed to remain idle before it expires. If you want them
+    | to immediately expire on the browser closing, set that option.
+    |
+    */
+    'lifetime' => env('TOKEN_LIFETIME','120'),
 ];
